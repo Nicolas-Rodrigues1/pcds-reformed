46fc4c8a2f44704501870f7205f3eca06c99b67b
Nicolas-Rodrigues1 <nicolas.rodrigues@ds2u.com.br>
2024-07-26 14:37:17 -0300

    Criar cliente.

--- src/app/autenticacao/clientes/criar-cliente/criar-cliente.component.ts
+++ src/app/autenticacao/clientes/criar-cliente/criar-cliente.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from '../cliente';
+import { ClienteService } from '../cliente.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-cliente',
   templateUrl: './criar-cliente.component.html',
   styleUrls: ['./criar-cliente.component.scss']
 })
-export class CriarClienteComponent {
+export class CriarClienteComponent implements OnInit{
 
+  cliente: Cliente = {
+    id: '',
+    nome: '',
+    nascimento: '',
+    cpf: '',
+    cep: '',
+    telefone: '',
+    email: '',
+    genero: ''
+  }
+
+  constructor(
+    private clienteService: ClienteService,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+  }
+
+  criarCliente(){
+    this.clienteService.criar(this.cliente).subscribe(()=>{
+      this.router.navigate(['/auth/clientes'])
+    })
+  }
+
+  cancelar(){
+    this.router.navigate(['/auth/clientes'])
+  }
 }
